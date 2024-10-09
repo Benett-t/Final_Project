834e0a656031bcde65c45c7d5a8914939093842a
MateP777 <palinnkas.mate@gmail.com>
2024-10-09 16:15:53 +0200

    Render template

--- Final_Project/app.py
+++ Final_Project/app.py
@@ -14,3 +14,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 # Initilise session
 Session(app)
+
+@app.route("/")
+def index():
+    #Landing page after login
+
+    return render_template("index.html")
