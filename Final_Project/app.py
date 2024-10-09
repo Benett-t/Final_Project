0faddcd414a6190f3d867aa5055e679ec1df29d1
Benett-t <Benettpalinkas@gmail.com>
2024-10-09 17:01:19 +0200

    defining login

--- Final_Project/app.py
+++ Final_Project/app.py
@@ -16,6 +16,14 @@ app.config["SESSION_TYPE"] = "filesystem"
 # Initilise session
 Session(app)
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
 def index():
     #Landing page after login
@@ -32,3 +40,17 @@ def login_required(f):
             return redirect("/login")
     return decorated_function
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # TODO
+    if session.get("user_id") is not None:
+        # if we are logged in then prompt to logout
+        return render_template("logout.html")
+    else:
+        if request.method == "GET":
+            # base case
+            return render_template("login.html")
+        # else if POST.
+        else:
+            pass
+            # TODO first have to make login.html
